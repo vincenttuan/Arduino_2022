53876212b0678decfae185c421178078cc703d74
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-04-09 16:14:49 +0800

    Add files via upload

--- Serial_Control_LED2/Serial_Control_LED2.ino
+++ Serial_Control_LED2/Serial_Control_LED2.ino
@@ -4,12 +4,46 @@
 // 輸入: 3 LED_PIN_13 ON,  LED_PIN_12 ON
 // 輸入: 4 LED_PIN_13 OFF, LED_PIN_12 OFF
 // 輸入: 5 LED_PIN_13 與 LED_PIN_12 交替閃爍 (delay(200);)
-void setup() {
-  // put your setup code here, to run once:
+const int LED_PIN_13 = 13;
+const int LED_PIN_12 = 12;
+int n = 0;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_PIN_13, OUTPUT);
+  pinMode(LED_PIN_12, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(Serial.available() > 0) {
+    n = Serial.read();
+  }
+  runLED();
+  delay(10);
+}
+void runLED() {
+    switch(n) {
+      case 49: // 1
+        digitalWrite(LED_PIN_13, HIGH);
+        digitalWrite(LED_PIN_12, LOW);
+        break;  
+      case 50: // 2
+        digitalWrite(LED_PIN_13, LOW);
+        digitalWrite(LED_PIN_12, HIGH);
+        break;  
+      case 51: // 3
+        digitalWrite(LED_PIN_13, HIGH);
+        digitalWrite(LED_PIN_12, HIGH);
+        break;  
+      case 52: // 4
+        digitalWrite(LED_PIN_13, LOW);
+        digitalWrite(LED_PIN_12, LOW);
+        break;  
+      case 53: // 5
+        int value = !digitalRead(LED_PIN_13);
+        digitalWrite(LED_PIN_13, value);
+        digitalWrite(LED_PIN_12, !value);
+        delay(200);
+        break;
+    }
 }
